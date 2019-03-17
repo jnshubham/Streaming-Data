0e433619ab10d15a08e05c162b400a87ce8b0571
Shubham Jain <43238222+workingSource@users.noreply.github.com>
2019-03-17 15:11:02 +0530

    Updated the code to generate messages continuously

--- kafka.producer/src/main/java/kafka/producer/RunApp.java
+++ kafka.producer/src/main/java/kafka/producer/RunApp.java
@@ -11,31 +11,28 @@ public class RunApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		runProducer();
+		try {
+			runProducer();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("An Error Occured:"+e.getMessage());
+		}
 	}
-	static void runProducer() {
+	static void runProducer() throws InterruptedException, IOException, ExecutionException {
 		Producer<Long, String> producer = DataProducer.createProducer();
 		int id=0;
 		GenerateXMLData xmlD=new GenerateXMLData();
-		for (int index = 0; index < 100; index++) {
-			try {
+		while(true) {
+			for (int index = 0; index < 100; index++) {
+
 				String data=xmlD.generateXML(Integer.toString(id++));
 				ProducerRecord<Long, String> record = new ProducerRecord<Long, String>("MunicipalXML",data);
 				RecordMetadata metadata = producer.send(record).get();
 				System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
 				+ " with offset " + metadata.offset());
-			} 
-			catch (ExecutionException e) {
-				System.out.println("Error in sending record");
-				System.out.println(e);
-			} 
-			catch (InterruptedException e) {
-				System.out.println("Error in sending record");
-				System.out.println(e);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+			Thread.sleep(60);
 		}
 	}
 
